68b4925439e89b11ca9aaa251a152b97c2d9ff7d
HzaRashid <hamzarashid.mail@gmail.com>
2023-09-07 01:07:06 -0400

    misc

--- server/index.js
+++ server/index.js
@@ -89,6 +89,13 @@ app.use('/sprints', require('./routes/SprintRoute'))
 app.use('/projects', require('./routes/ProjectRoute'))
 app.use('/comments', require('./routes/CommentRoute'))
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+}
 
 
 const PORT = process.env.PORT || 4050;
