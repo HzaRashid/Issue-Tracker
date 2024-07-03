55d758e4c8e13309253bd54519b79deda6917ea5
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-03 16:49:57 -0400

    misc

--- server/index.js
+++ server/index.js
@@ -57,6 +57,7 @@ app.use(session({
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       httpOnly: false,
+      sameSite: 'none'
     },
     store: redisStore,
     resave: false,
