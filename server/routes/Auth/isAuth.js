132b3a2eee5d758fdf0d1a25b8c34a8ef2f83ba7
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-03 21:06:19 -0400

    misc

--- server/routes/Auth/isAuth.js
+++ server/routes/Auth/isAuth.js
@@ -26,7 +26,6 @@ const isAuth = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return res.status(400).json({
       message: 'user not authenticated, nor authorized to make changes',
-      foo: req.ip
     })
   } next()
 }
