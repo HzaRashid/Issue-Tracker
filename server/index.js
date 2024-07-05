39bb8940254dec6a98f5a071ae4b900931171f51
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-05 16:15:43 -0400

    misc

--- server/index.js
+++ server/index.js
@@ -88,5 +88,5 @@ if (process.env.NODE_ENV == "production") {
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0",
+app.listen(PORT,
     () => console.log( `connected to http://localhost:${PORT}` ))
