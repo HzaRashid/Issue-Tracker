a6736135df3b2ec59c0c67a2064d738d90cfbe97
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-10 18:44:04 -0400

    misc

--- init-encrypt-server.sh
+++ init-encrypt-server.sh
@@ -34,28 +34,28 @@ echo "${COMPOSE_FNAME}"
 echo yeryeryeryyryeyryryr
 
 
-# if [ -d "$data_path" ]; then
-#   exit
-# fi
+if [ -d "$data_path" ]; then
+  exit
+fi
 
 
-# if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-#   echo "### Downloading recommended TLS parameters ..."
-#   mkdir -p "$data_path/conf"
-#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-#   echo
-# fi
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### Downloading recommended TLS parameters ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
 
-# echo "### Creating dummy certificate for $domains ..."
-# path="/etc/letsencrypt/live/$domains"
-# mkdir -p "$data_path/conf/live/$domains"
-# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-#     -keyout '$path/privkey.pem' \
-#     -out '$path/fullchain.pem' \
-#     -subj '/CN=localhost'" certbot
-# echo
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
 
 
 # echo "### Starting nginx reverse-proxy ..."
