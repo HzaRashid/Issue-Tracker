2a38471ac5ebc378b7b6190904ede6bae7535275
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-10 16:36:33 -0400

    misc

--- init-encrypt-server.sh
+++ init-encrypt-server.sh
@@ -67,43 +67,43 @@ sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
 echo
 
 
-echo "### Starting nginx reverse-proxy ..."
-sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
-echo
+# echo "### Starting nginx reverse-proxy ..."
+# sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
+# echo
 
-echo "### Deleting dummy certificate for $domains ..."
-sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+# echo "### Deleting dummy certificate for $domains ..."
+# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+#   rm -Rf /etc/letsencrypt/live/$domains && \
+#   rm -Rf /etc/letsencrypt/archive/$domains && \
+#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+# echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+# echo "### Requesting Let's Encrypt certificate for $domains ..."
+# #Join $domains to -d args
+# domain_args=""
+# for domain in "${domains[@]}"; do
+#   domain_args="$domain_args -d $domain"
+# done
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
+# # Select appropriate email arg
+# case "$email" in
+#   "") email_arg="--register-unsafely-without-email" ;;
+#   *) email_arg="--email $email" ;;
+# esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+# # Enable staging mode if needed
+# if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
+# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+#   certbot certonly --webroot -w /var/www/certbot \
+#     $staging_arg \
+#     $email_arg \
+#     $domain_args \
+#     --rsa-key-size $rsa_key_size \
+#     --agree-tos \
+#     --force-renewal" certbot
+# echo
 
-echo "### Reloading nginx reverse-proxy ..."
-sudo docker compose -f $COMPOSE_FNAME exec reverse-proxy reverse-proxy -s reload
+# echo "### Reloading nginx reverse-proxy ..."
+# sudo docker compose -f $COMPOSE_FNAME exec reverse-proxy reverse-proxy -s reload
