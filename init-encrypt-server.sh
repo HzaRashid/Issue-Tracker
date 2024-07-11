420abc8ff5e8cf981059384e122af9583c81fe1f
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-10 23:21:51 -0400

    misc

--- init-encrypt-server.sh
+++ init-encrypt-server.sh
@@ -38,67 +38,67 @@ staging=1 # Set to 1 if you're testing your setup to avoid hitting request limit
 echo "shshsh $API_DOMAIN "
 echo "shshsh $COMPOSE_FNAME"
 
-# if [ -d "$data_path" ]; then exit; fi
+if [ -d "$data_path" ]; then exit; fi
 
-# if  [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || \ 
-#     [ ! -e "$data_path/conf/ssl-dhparams.pem" ]
-# then
-#   echo "### Downloading recommended TLS parameters ..."
-#   sudo mkdir -p "$data_path/conf"
-#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/\
-#   certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-#   curl -s https://raw.githubusercontent.com/certbot/certbot/master/\
-#   certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-#   echo
-# fi
+if  [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || \ 
+    [ ! -e "$data_path/conf/ssl-dhparams.pem" ]
+then
+  echo "### Downloading recommended TLS parameters ..."
+  sudo mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/\
+  certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/\
+  certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo
+fi
 
-# echo "### Creating dummy certificate for $domains ..."
-# path="/etc/letsencrypt/live/$domains"
-# mkdir -p "$data_path/conf/live/$domains"
-# docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-#     -keyout '$path/privkey.pem' \
-#     -out '$path/fullchain.pem' \
-#     -subj '/CN=localhost'" certbot
-# echo
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
 
-# echo "### Starting nginx reverse-proxy ..."
-# docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
-# echo
+echo "### Starting nginx reverse-proxy ..."
+docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
+echo
 
-# echo "### Deleting dummy certificate for $domains ..."
-# docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   rm -Rf /etc/letsencrypt/live/$domains && \
-#   rm -Rf /etc/letsencrypt/archive/$domains && \
-#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-# echo
+echo "### Deleting dummy certificate for $domains ..."
+docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
 
-# echo "### Requesting Let's Encrypt certificate for $domains ..."
-# #Join $domains to -d args
-# domain_args=""
-# for domain in "${domains[@]}"; do
-#   domain_args="$domain_args -d $domain"
-# done
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
 
 
-# # Select appropriate email arg
-# case "$email" in
-#   "") email_arg="--register-unsafely-without-email" ;;
-#   *) email_arg="--email $email" ;;
-# esac
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
 
-# # Enable staging mode if needed
-# if [ $staging != "0" ]; then staging_arg="--staging"; fi
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-# docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   certbot certonly --webroot -w /var/www/certbot \
-#     $staging_arg \
-#     $email_arg \
-#     $domain_args \
-#     --rsa-key-size $rsa_key_size \
-#     --agree-tos \
-#     --force-renewal" certbot
-# echo
+docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
 
-# echo "### Reloading nginx reverse-proxy ..."
-# docker compose -f $COMPOSE_FNAME exec reverse-proxy reverse-proxy -s reload
+echo "### Reloading nginx reverse-proxy ..."
+docker compose -f $COMPOSE_FNAME exec reverse-proxy reverse-proxy -s reload
