bf88d024ceca8db8dbd914f079c449917f55b9c3
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-10 23:19:40 -0400

    misc

--- init-encrypt-server.sh
+++ init-encrypt-server.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
+docker compose &>/dev/null # sending output to /dev/null because we don't want it printed
 
-# if ! [ -x "$(command -v docker compose)" ]; then
-#   # Install Compose plugin for Ubuntu via Docker's Repository:
-#   # Add Docker's official GPG key:
-#   apt-get update
-#   apt-get install ca-certificates curl
-#   install -m 0755 -d /etc/apt/keyrings
-#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#   chmod a+r /etc/apt/keyrings/docker.asc
-#   # Add the repository to Apt sources:
-#   echo \
-#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-#     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#     tee /etc/apt/sources.list.d/docker.list > /dev/null
-#   apt-get update
-#   # install the latest version
-#   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-#   # Update the package index, and install the latest version of docker compose
-#   apt-get update
-#   apt-get install docker-compose-plugin
-#   # Verify that docker compose is installed correctly by checking the version.
-#   docker compose version
-# fi
+if [ $? -eq 0 ]; then
+    echo "INSTALLED"
+else
+    echo "NOT INSTALLED"
+fi
+if ! [ -x "$(command -v docker compose)" ]; then
+  # Install Compose plugin for Ubuntu via Docker's Repository:
+  # Add Docker's official GPG key:
+  apt-get update
+  apt-get install ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  # install the latest version
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # Update the package index, and install the latest version of docker compose
+  apt-get update
+  apt-get install docker-compose-plugin
+  # Verify that docker compose is installed correctly by checking the version.
+  docker compose version
+fi
 
 
 domains=($API_DOMAIN)
