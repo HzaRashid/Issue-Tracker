4da6412ed5b151480d23c9d25be354b8cc13dd31
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-12 15:34:40 -0400

    misc

--- scripts/server/ssl-cert.sh
+++ scripts/server/ssl-cert.sh
@@ -29,7 +29,7 @@ else
 fi
 
 
-# pre_cert_conf_path=$(pwd)/server-configs/pre-cert/config.conf
+# pre_cert_conf_path=$(pwd)/server-configs/pre-cert/config.conf 
 # post_cert_conf_path=$(pwd)/server-configs/post-cert/config.conf
 
 # proxy_ctr_conf_path=/etc/nginx/conf.d/config.conf 
