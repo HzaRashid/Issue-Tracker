1fc8b24e87e57242e24797110988ea1ce8a7c21b
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-12 12:40:47 -0400

    misc

--- scripts/init-encrypt-server.sh
+++ scripts/init-encrypt-server.sh
@@ -51,12 +51,10 @@ then
   echo foo 1
 
   sudo curl -o "$data_path/conf/options-ssl-nginx.conf" \
-  https://raw.githubusercontent.com/certbot/certbot/master\
-  /certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+  https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
 
   sudo curl -o  "$data_path/conf/ssl-dhparams.pem" \
-  https://raw.githubusercontent.com/certbot/certbot/master\
-  /certbot/certbot/ssl-dhparams.pem
+  https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem
 
   sslconf=$(ls "$data_path/conf")
   echo $sslconf
@@ -66,71 +64,71 @@ then
 fi
 
 
-echo "### Creating dummy certificate for $domains ..."
+# echo "### Creating dummy certificate for $domains ..."
 
-path="/etc/letsencrypt/live/$domains"
+# path="/etc/letsencrypt/live/$domains"
 
-sudo mkdir -p "$data_path/conf/live/$domains"
+# sudo mkdir -p "$data_path/conf/live/$domains"
 
-sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
-
-
-echo "### Starting nginx reverse-proxy ..."
-sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
-echo
+# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+#     -keyout '$path/privkey.pem' \
+#     -out '$path/fullchain.pem' \
+#     -subj '/CN=localhost'" certbot
+# echo
 
 
-echo "### Deleting dummy certificate for $domains ..."
-sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+# echo "### Starting nginx reverse-proxy ..."
+# sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
+# echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+# echo "### Deleting dummy certificate for $domains ..."
+# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+#   rm -Rf /etc/letsencrypt/live/$domains && \
+#   rm -Rf /etc/letsencrypt/archive/$domains && \
+#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+# echo
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+# echo "### Requesting Let's Encrypt certificate for $domains ..."
+# #Join $domains to -d args
+# domain_args=""
+# for domain in "${domains[@]}"; do
+#   domain_args="$domain_args -d $domain"
+# done
 
-cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
-echo $cert
-echo foo 2
+# # Select appropriate email arg
+# case "$email" in
+#   "") email_arg="--register-unsafely-without-email" ;;
+#   *) email_arg="--email $email" ;;
+# esac
 
-sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-  certbot -v certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
+# # Enable staging mode if needed
+# if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
-echo $cert
+# cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
+# echo $cert
+# echo foo 2
 
-echo "### Reloading nginx reverse-proxy ..."
-sudo docker compose -f $COMPOSE_FNAME exec reverse-proxy nginx -s reload 
-# sudo docker rm -f reverse-proxy || true 
-# sudo bash -c 'echo y | docker system prune'
-# sudo docker cp $post_cert_conf_path reverse-proxy:$proxy_ctr_conf_path
-# sudo docker exec reverse-proxy nginx -s reload 
+# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+#   certbot -v certonly --webroot -w /var/www/certbot \
+#     $staging_arg \
+#     $email_arg \
+#     $domain_args \
+#     --rsa-key-size $rsa_key_size \
+#     --agree-tos \
+#     --force-renewal" certbot
+# echo
 
-# sudo docker compose -f $COMPOSE_FNAME up --no-recreate -d
+# cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
+# echo $cert
+
+# echo "### Reloading nginx reverse-proxy ..."
+# sudo docker compose -f $COMPOSE_FNAME exec reverse-proxy nginx -s reload 
+# # sudo docker rm -f reverse-proxy || true 
+# # sudo bash -c 'echo y | docker system prune'
+# # sudo docker cp $post_cert_conf_path reverse-proxy:$proxy_ctr_conf_path
+# # sudo docker exec reverse-proxy nginx -s reload 
+
+# # sudo docker compose -f $COMPOSE_FNAME up --no-recreate -d
