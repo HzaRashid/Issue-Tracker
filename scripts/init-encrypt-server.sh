156531087be54acc34a7519ae570b24937ed373e
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-12 12:55:18 -0400

    misc

--- scripts/init-encrypt-server.sh
+++ scripts/init-encrypt-server.sh
@@ -63,66 +63,94 @@ then
   echo
 fi
 
+echo CHECK DELETED 
+cat $data_path/conf/options-ssl-nginx.conf
+echo CHECK DELETED 
 
-# echo "### Creating dummy certificate for $domains ..."
+echo "### Creating dummy certificate for $domains ..."
 
-# path="/etc/letsencrypt/live/$domains"
+path="/etc/letsencrypt/live/$domains"
 
-# sudo mkdir -p "$data_path/conf/live/$domains"
+sudo mkdir -p "$data_path/conf/live/$domains"
 
-# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-#     -keyout '$path/privkey.pem' \
-#     -out '$path/fullchain.pem' \
-#     -subj '/CN=localhost'" certbot
-# echo
-
-
-# echo "### Starting nginx reverse-proxy ..."
-# sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
-# echo
+sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
 
 
-# echo "### Deleting dummy certificate for $domains ..."
-# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   rm -Rf /etc/letsencrypt/live/$domains && \
-#   rm -Rf /etc/letsencrypt/archive/$domains && \
-#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-# echo
+echo CHECK DELETED 
+cat server-configs/certbot/conf/options-ssl-nginx.conf
+echo CHECK DELETED 
 
 
-# echo "### Requesting Let's Encrypt certificate for $domains ..."
-# #Join $domains to -d args
-# domain_args=""
-# for domain in "${domains[@]}"; do
-#   domain_args="$domain_args -d $domain"
-# done
+echo "### Starting nginx reverse-proxy ..."
+sudo docker compose -f $COMPOSE_FNAME up --force-recreate -d reverse-proxy
+echo
 
-# # Select appropriate email arg
-# case "$email" in
-#   "") email_arg="--register-unsafely-without-email" ;;
-#   *) email_arg="--email $email" ;;
-# esac
 
-# # Enable staging mode if needed
-# if [ $staging != "0" ]; then staging_arg="--staging"; fi
+echo CHECK DELETED 
+cat $data_path/conf/options-ssl-nginx.conf
+echo CHECK DELETED 
 
-# cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
-# echo $cert
-# echo foo 2
 
-# sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
-#   certbot -v certonly --webroot -w /var/www/certbot \
-#     $staging_arg \
-#     $email_arg \
-#     $domain_args \
-#     --rsa-key-size $rsa_key_size \
-#     --agree-tos \
-#     --force-renewal" certbot
-# echo
+echo "### Deleting dummy certificate for $domains ..."
+sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
 
-# cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
-# echo $cert
+
+
+echo CHECK DELETED 
+cat $data_path/conf/options-ssl-nginx.conf
+echo CHECK DELETED 
+
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
+echo $cert
+echo foo 2
+
+sudo docker compose -f $COMPOSE_FNAME run --rm --entrypoint "\
+  certbot -v certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
+
+cert=$(ls "./server-configs/certbot/conf/live/$API_DOMAIN")
+echo $cert
+
+
+
+echo CHECK DELETED 
+cat $data_path/conf/options-ssl-nginx.conf
+echo CHECK DELETED 
+
+
+
 
 # echo "### Reloading nginx reverse-proxy ..."
 # sudo docker compose -f $COMPOSE_FNAME exec reverse-proxy nginx -s reload 
