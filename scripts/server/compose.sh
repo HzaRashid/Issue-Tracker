545c3bf2c5dc6565bc4f9675236d6d3c14c2b42e
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-14 16:19:03 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-
-post_cert_conf_path=./server-configs/$CONFIG_DIR/post-cert.conf
-pre_cert_conf_path=./server-configs/$CONFIG_DIR/pre-cert.conf
-export data_path="./server-configs/certbot"
 scripts_path="./scripts/server"
 
-echo "foo $CONFIG_DIR foo"
+
+post_cert_conf_path=./server-configs/proxy/post-cert.conf
+pre_cert_conf_path=./server-configs/proxy/pre-cert.conf
+export data_path="./server-configs/certbot"
+
 
 if [ ! -d "$data_path" ]; then  # ssl cert not found
   chmod +x $scripts_path/ssl-cert.sh
   $scripts_path/ssl-cert.sh
 fi
 
-echo "### Compose Up ..."
 
+echo "### Compose Up ..."
 cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
 # sudo docker compose -f $COMPOSE_FNAME up -d
