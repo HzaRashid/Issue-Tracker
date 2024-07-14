7f4923a4a22f80825a6f56a926d4c9bf940ff3fc
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-14 15:55:06 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 
-scripts_path="./scripts/server"
-
-if [ -x "$(command -v docker)" ]; then
-    echo "Docker installed"
-    # command
-else
-  chmod +x $scripts_path/install-docker.sh
-  $scripts_path/install-docker.sh
-fi
 
 post_cert_conf_path=./server-configs/$CONFIG_DIR/post-cert.conf
 pre_cert_conf_path=./server-configs/$CONFIG_DIR/pre-cert.conf
 export data_path="./server-configs/certbot"
+scripts_path="./scripts/server"
 
 echo "foo $CONFIG_DIR foo"
-
 
 if [ ! -d "$data_path" ]; then  # ssl cert not found
   chmod +x $scripts_path/ssl-cert.sh
   $scripts_path/ssl-cert.sh
 fi
-
 
 echo "### Compose Up ..."
 
