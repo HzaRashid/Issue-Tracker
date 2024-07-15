abf09122c54bde5be8a5667aa3dae8b29f337744
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-15 15:10:31 -0400

    misc

---
+++ scripts/server/install-docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Only recommended for testing and development environments.
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker already installed"
+else
+    echo "Install docker"
+    sudo curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
