84217f153f1bc1ade9de2df1f9f87f9df658cfad
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-15 11:45:31 -0400

    misc

--- scripts/server/install-docker.sh
+++ scripts/server/install-docker.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 
-# Install Compose plugin for Ubuntu via Docker's Repository:
-
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-# install the latest version
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Update the package index, and install the latest version of docker compose
-sudo apt-get update
-sudo apt-get install docker-compose-plugin
-
-# Verify that docker compose is installed correctly by checking the version.
-docker compose version
+# Only recommended for testing and development environments.
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker installed"
+else
+    echo "Install docker"
+    sudo curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
