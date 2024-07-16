f5b32846b3e71d6466a5e6870b4ed702deaa471a
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 14:34:39 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-scripts_path="./scripts/server"
+scripts_path=./scripts/server
+configs_path=./server-configs/$CONFIG_TYPE
 
-
-post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
-pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
+post_cert_conf_path=$configs_path/post-cert.conf
+pre_cert_conf_path=$configs_path/pre-cert.conf
 export data_path="./server-configs/certbot"
 
 if [ "$CONFIG_TYPE" = "proxied" ]; then
-  envsubst < $pre_cert_conf_path
-  envsubst < $post_cert_conf_path
+  touch $configs_path/temp.conf
+  envsubst < $pre_cert_conf_path > $configs_path/temp.conf && mv $configs_path/temp.conf $pre_cert_conf_path
+  envsubst < $post_cert_conf_path > $configs_path/temp.conf && mv $configs_path/temp.conf $post_cert_conf_path
 fi
 
-if [ ! -d "$data_path" ]; then  # ssl cert not found
-  chmod +x $scripts_path/ssl-cert.sh
-  $scripts_path/ssl-cert.sh
-fi
+# if [ ! -d "$data_path" ]; then  # ssl cert not found
+#   chmod +x $scripts_path/ssl-cert.sh
+#   $scripts_path/ssl-cert.sh
+# fi
 
 
-echo "### Compose Up ..."
-cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
+# echo "### Compose Up ..."
+# cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
 
-sudo docker compose -f $COMPOSE_FNAME up -d 
+# sudo docker compose -f $COMPOSE_FNAME up -d 
