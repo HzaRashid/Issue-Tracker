85e5a77bd9b03fe4882857f0d842285ee7ad3061
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 19:47:05 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -9,9 +9,6 @@ export data_path="./server-configs/certbot"
 
 # if [ "$CONFIG_TYPE" = "proxied" ]; then
 subs=""
-# for domain in "${domains[@]}"; do
-#   domain_args="$domain_args -d $domain"
-# done
 while IFS= read -r line; do
   value=${line#*=}
   name=${line%%=*}
@@ -23,9 +20,9 @@ done < .env.proxy
 echo $subs
 
 tmpfile=$(mktemp)
-envsubst < $pre_cert_conf_path > $tmpfile && mv $tmpfile $pre_cert_conf_path
+envsubst $subs < $pre_cert_conf_path > $tmpfile && mv $tmpfile $pre_cert_conf_path
 tmpfile=$(mktemp)
-envsubst  < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
+envsubst $subs < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
 
 # fi
 
@@ -36,9 +33,9 @@ fi
 
 
 echo "### Compose Up ..."
-cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
-if [ "$CONFIG_TYPE" = "proxied" ]; then
-  sudo docker compose -f $COMPOSE_FNAME up -d 
+# cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
+# if [ "$CONFIG_TYPE" = "proxied" ]; then
+#   sudo docker compose -f $COMPOSE_FNAME up -d 
 
-fi
-# sudo docker compose -f $COMPOSE_FNAME up -d 
+# fi
+sudo docker compose -f $COMPOSE_FNAME up -d 
