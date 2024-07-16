c69a7f84fa0977ffccfe98a734ae0533002cbafb
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 14:38:14 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-scripts_path=./scripts/server
-configs_path=./server-configs/$CONFIG_TYPE
+scripts_path="./scripts/server"
 
-post_cert_conf_path=$configs_path/post-cert.conf
-pre_cert_conf_path=$configs_path/pre-cert.conf
+
+post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
+pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/certbot"
 
 if [ "$CONFIG_TYPE" = "proxied" ]; then
-  touch $configs_path/temp.conf
-  envsubst < $pre_cert_conf_path > $configs_path/temp.conf && mv $configs_path/temp.conf $pre_cert_conf_path
-  envsubst < $post_cert_conf_path > $configs_path/temp.conf && mv $configs_path/temp.conf $post_cert_conf_path
+  envsubst < $pre_cert_conf_path | sponge $pre_cert_conf_path
+  envsubst < $post_cert_conf_path | sponge $post_cert_conf_path
 fi
 
 # if [ ! -d "$data_path" ]; then  # ssl cert not found
