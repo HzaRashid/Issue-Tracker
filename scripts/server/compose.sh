9275104b61952856900c30172f6035e5bb07f2e9
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 14:18:31 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -7,6 +7,10 @@ post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
 pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/certbot"
 
+if [ "$CONFIG_TYPE" = "proxied" ]; then
+  envsubst < $pre_cert_conf_path
+  envsubst < $post_cert_conf_path
+fi
 
 if [ ! -d "$data_path" ]; then  # ssl cert not found
   chmod +x $scripts_path/ssl-cert.sh
@@ -16,4 +20,5 @@ fi
 
 echo "### Compose Up ..."
 cp $post_cert_conf_path $pre_cert_conf_path # do not move/remove - source file might be updated
+
 sudo docker compose -f $COMPOSE_FNAME up -d 
