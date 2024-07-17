fb15dbe32628d4f187c5b27192b1ce29b417989c
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 20:05:54 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -7,7 +7,7 @@ post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
 pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/certbot"
 
-# if [ "$CONFIG_TYPE" = "proxied" ]; then
+
 subs=""
 while IFS= read -r line; do
   value=${line#*=}
@@ -24,7 +24,7 @@ envsubst "$subs" < $pre_cert_conf_path > $tmpfile && mv $tmpfile $pre_cert_conf_
 tmpfile=$(mktemp)
 envsubst "$subs" < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
 
-# fi
+
 
 # if [ ! -d "$data_path" ]; then  # ssl cert not found
 #   chmod +x $scripts_path/ssl-cert.sh
