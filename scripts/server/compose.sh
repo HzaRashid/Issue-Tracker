c582e14591f9a7bf8e6f5b04a99b3cd2ed328aa9
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 22:14:26 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -7,20 +7,22 @@ post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
 pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/certbot"
 
-
+: '
+extract the key-val pairs 
+to hydrate in nginx configuration files
+'
 subs=""
 unsets=""
-IFS=$'\n' 
-for line in $(cat .env.proxy)
-do
+IFS=$'\n'
+proxy_env=$(cat .env.proxy)
+for line in $proxy_env; do
   key=${line%%=*}
   subs="$subs '\$$key'"
   unsets="$unsets $key"
 done
 unset IFS
 
-export $(cat .env.proxy | xargs)
-
+export $($proxy_env | xargs) # load key-val assignments into environment
 tmpfile=$(mktemp)
 envsubst "$subs" < $pre_cert_conf_path > $tmpfile && mv $tmpfile $pre_cert_conf_path
 tmpfile=$(mktemp)
