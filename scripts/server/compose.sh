a2dac8a61c5445a41218d516058e828d6cb924d8
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-16 23:45:52 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -2,15 +2,12 @@
 
 scripts_path="./scripts/server"
 
-
 post_cert_conf_path=./server-configs/$CONFIG_TYPE/post-cert.conf
 pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/certbot"
 
-: '
-extract the key-value pairs 
-to hydrate in nginx configuration files
-'
+
+# extract key-value pairs that will hydrate nginx config files
 subs=""
 unsets=""
 IFS=$'\n'
@@ -24,7 +21,6 @@ unset IFS
 export $(cat .env.proxy | xargs) # load key-value assignments into environment
 tmpfile=$(mktemp)
 envsubst "$subs" < $pre_cert_conf_path > $tmpfile && mv $tmpfile $pre_cert_conf_path
-tmpfile=$(mktemp)
 envsubst "$subs" < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
 
 
