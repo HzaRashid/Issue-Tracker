e37a0cadfde1166ba98e4b64510bf121de4d8d2f
HzaRashid <hamzarashid.mail@gmail.com>
2024-07-20 14:59:16 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -7,20 +7,20 @@ pre_cert_conf_path=./server-configs/$CONFIG_TYPE/pre-cert.conf
 export data_path="./server-configs/ssl"
 
 
-# extract key-value pairs that will hydrate nginx config files
-subs=""
-unsets=""
-IFS=$'\n'
-for line in $(cat .env.proxy); do
-  key=${line%%=*}
-  subs="$subs '\$$key'"
-  unsets="$unsets $key"
-done
-unset IFS
+# # extract key-value pairs that will hydrate nginx config files
+# subs=""
+# unsets=""
+# IFS=$'\n'
+# for line in $(cat .env.proxy); do
+#   key=${line%%=*}
+#   subs="$subs '\$$key'"
+#   unsets="$unsets $key"
+# done
+# unset IFS
 
-export $(cat .env.proxy | xargs) # load key-value assignments into environment
-tmpfile=$(mktemp)
-envsubst "$subs" < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
+# export $(cat .env.proxy | xargs) # load key-value assignments into environment
+# tmpfile=$(mktemp)
+# envsubst "$subs" < $post_cert_conf_path > $tmpfile && mv $tmpfile $post_cert_conf_path
 
 
 if [ ! -d "$data_path" ]; then  # ssl cert not found
@@ -36,4 +36,4 @@ fi
 echo "### Compose Up ..."
 sudo docker compose -f $COMPOSE_FNAME up -d
 
-unset "$unsets"
+# unset "$unsets"
