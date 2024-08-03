973d9a70a895a0e2506d30cbd0a4d7c2c47cd24e
HzaRashid <hamzarashid.mail@gmail.com>
2024-08-02 20:42:58 -0400

    misc

--- scripts/server/compose.sh
+++ scripts/server/compose.sh
@@ -9,5 +9,4 @@ sudo -E bash -c 'echo "$CERT" > $data_path/certs/flow-cert.pem'
 sudo -E bash -c 'echo "$CERT_KEY" > $data_path/private/flow-key.pem'
 
 echo "### Compose Up ..."
-docker-compose pull
 sudo docker compose -f $COMPOSE_FNAME up -d
